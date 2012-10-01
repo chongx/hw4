8f41c49e4924aa68b86a1c9fc906a4a43295fc4c
Chong Xie <chongx@andrew.cmu.edu>
2012-09-30 23:35:02 -0400

    Awesomeness.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,36 @@
-var nextId = 0;
+var tools = {
+  NONE: 0,
+  RECTANGLE : 1
+};
+var user = {
+  nextId: 0,
+  genId: function() {
+    var id = user.nextId;
+    user.nextId++;
+    return id;
+  },
+  currentElem: null,
+  currentTool: tools.RECTANGLE,
+  setCurrentElem: function(id) {
+    user.currentElem = id;
+    var settings = $('#elem-settings');
+    settings.detach();
+    if(id == null) {
+      $('body').append(settings);  
+      settings.hide();
+    } else {
+      $('#' + id).append(settings);
+      settings.show();
+    }
+  }
+};
+
+
 var collection = new PaneCollection();
 
 $(function() {
-  $('#0').on('click', collection.panes[0], paneClickHandler);
+  collection.panes[0].startHandlers();
 });
-
 function PaneCollection() {
   this.current = 0;
   this.panes = [new Pane()];
@@ -14,7 +40,7 @@ function PaneCollection() {
 
 PaneCollection.prototype.isCurrent = function(id) {
   return id === this.ordering[this.current];
-}
+};
 
 PaneCollection.prototype.isNext = function(id) {
   // Current one is last one in the collection
@@ -22,7 +48,7 @@ PaneCollection.prototype.isNext = function(id) {
     return false;
   }
   return id === this.ordering[this.current + 1];
-}
+};
 
 PaneCollection.prototype.isPrev = function(id) {
   // Current one is first one in the collection
@@ -30,65 +56,140 @@ PaneCollection.prototype.isPrev = function(id) {
     return false;
   }
   return id === this.ordering[this.current - 1];
-}
+};
 
 PaneCollection.prototype.getNext = function() {
   if(this.current === this.ordering.length - 1) {
     return null;
   }
   return this.panes[this.ordering[this.current + 1]];
-}
+};
 
 PaneCollection.prototype.getPrev = function() {
   if(this.current === 0) {
     return null;
   }
   return this.panes[this.ordering[this.current - 1]];
-}
+};
 
 PaneCollection.prototype.getCurrent = function() {
   return this.panes[this.ordering[this.current]];
-}
+};
 
 PaneCollection.prototype.rotateLeft = function() {
   if(collection.getPrev() !== null) {
-    collection.getPrev().getElement().addClass('offleft');
+    collection.getPrev().getNode().addClass('offleft');
   }
-  collection.getCurrent().getElement().removeClass('center').addClass('left');
-  collection.getNext().getElement().removeClass('right').addClass('center');
+  collection.getCurrent().getNode().removeClass('center').addClass('left');
+  collection.getNext().getNode().removeClass('right').addClass('center');
   collection.current++;
   if(collection.getNext() !== null) {
-    collection.getNext().getElement().removeClass('offright');
+    collection.getNext().getNode().removeClass('offright');
     $('#new').hide();
   } else {
     $('#new').show();
   }
-}
+};
 
 PaneCollection.prototype.rotateRight = function() {
   if(collection.getNext() !== null) {
-    collection.getNext().getElement().addClass('offright');
+    collection.getNext().getNode().addClass('offright');
     $('#new').hide();
   } else {
     $('#new').show();
   }
-  collection.getCurrent().getElement().removeClass('center').addClass('right');
-  collection.getPrev().getElement().removeClass('left').addClass('center');
+  collection.getCurrent().getNode().removeClass('center').addClass('right');
+  collection.getPrev().getNode().removeClass('left').addClass('center');
   collection.current--;
   if(collection.getPrev() !== null) {
-    collection.getPrev().getElement().removeClass('offleft');
+    collection.getPrev().getNode().removeClass('offleft');
   }
-}
+};
 
 function Pane() {
-  this.id = nextId;
-  nextId++;
-  this.paneId = "#" + this.id;
+  this.id = user.genId();
+  this.elems = [];
 }
 
-Pane.prototype.getElement = function() {
+Pane.prototype.getNode = function() {
   return $('#' + this.id);
+};
+
+Pane.prototype.startHandlers = function() {
+  this.getNode().on('click', this, this.clickHandler);
+};
+
+Pane.prototype.clickHandler = function(e) {
+  if(!collection.preview) {
+    if(collection.isPrev(e.data.id)) {
+      collection.rotateRight();
+    } else if(collection.isNext(e.data.id)) {
+      collection.rotateLeft();
+    } else if(collection.isCurrent(e.data.id)) {
+      if(user.currentTool != tools.NONE) {
+        // CALCULATE PROPER OFFSETS
+        var offsetX = e.offsetX;
+        var offsetY = e.offsetY;
+        var target = e.target;
+        while(!$(target).hasClass('pane')) {
+          console.log(target);
+          offsetX += $(target).position().left;
+          offsetY += $(target).position().top;
+          target = target.parentNode;
+        }
+        e.data.addElement(user.currentTool, offsetX, offsetY);
+        user.currentTool = tools.NONE;
+      } else {
+        var target = e.target;
+        while(!$(target).hasClass('pane')) {
+          if($(target).hasClass("elem")) {
+            var id = $(target).attr('id');
+            e.data.elems[id].setFocus();
+            return;
+          }
+          target = target.parentNode;
+        }
+        user.setCurrentElem(null);
+      }
+    }
+  }
+  e.preventDefault();
+};
+
+Pane.prototype.addElement = function(tool, x, y) {
+  var elem = new Elem();
+  var node, nodeLeft, nodeTop;
+  switch(tool) {
+    case tools.RECTANGLE:
+      node = $('<div class="elem rectangle" id="' + elem.id + '"></div>');
+      nodeLeft = x / this.getNode().width() * 100;
+      nodeTop = y / this.getNode().height() * 100;
+      node.css('left', nodeLeft + "%")
+          .css('top', nodeTop + "%");
+      break;
+  }
+  elem.setPosition(nodeLeft, nodeTop);
+  this.getNode().append(node);
+  this.elems[elem.id] = elem;
+};
+
+function Elem(type, left, top) {
+  this.id = user.genId();
+  this.type = type;
 }
+
+Elem.prototype.setPosition = function(left, top) {
+  this.left = left;
+  this.top = top;
+};
+
+Elem.prototype.setFocus = function() {
+  user.setCurrentElem(this.id);
+};
+
+Elem.prototype.getNode = function() {
+  return $('#' + this.id);
+};
 
 function togglePreview() {
   if(collection.preview) {
@@ -109,35 +210,22 @@ function newPane() {
     return;
   }
   var currentPane = collection.getCurrent();
-  $(currentPane.paneId)
+  currentPane.getNode()
     .removeClass('center')
     .addClass('left');
   if(collection.panes.length > 1) {
-    $(collection.panes[collection.ordering[collection.current - 1]].paneId).addClass("offleft");
+    collection.panes[collection.ordering[collection.current - 1]].getNode().addClass("offleft");
   }
   var pane = new Pane();
   collection.panes.push(pane);
   collection.current++;
   collection.ordering.push(pane.id);
   var elem = $('<div class="pane right" id="' + pane.id + '"></div>');
-  elem.on('click', pane, paneClickHandler);
   $('body').append(elem);
   elem.delay(100).queue(function(next) {
     elem.removeClass('right');
     elem.addClass('center');
     next();
   });
-}
-
-function paneClickHandler(e) {
-  if(!collection.preview) {
-    if(collection.isPrev(e.data.id)) {
-      collection.rotateRight();
-    } else if(collection.isNext(e.data.id)) {
-      collection.rotateLeft();
-    } else if(collection.isCurrent(e.data.id)) {
-      
-    }
-  }
-  e.preventDefault();
+  pane.startHandlers();
 }
